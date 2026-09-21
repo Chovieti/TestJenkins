567f060edb1d22b30b6bd62b12f5553da82b453c
chovieti <Egor821@yandex.ru>
2026-09-21 11:43:21 +0300

    test push

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,16 @@
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
   @Test
   void simpleTest() {
     assertTrue(true);
+  }
+
+  @Test
+  void mySimpleTest() {
+    assertEquals(1, Integer.parseInt("1"));
   }
 }
